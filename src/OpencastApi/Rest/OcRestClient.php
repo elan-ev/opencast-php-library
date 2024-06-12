5621e077243832a7c162a0801660e23934863ace
FarbodZamani <53179227+ferishili@users.noreply.github.com>
2024-06-12 13:05:20 +0200

    Fix runWithRoles without api version in config (#26)
    * prevent overwrting args by using cloned obj, fixes #23
    
    * Update src/OpencastApi/Rest/OcRestClient.php
    
    Co-authored-by: reiferschris <34450887+reiferschris@users.noreply.github.com>
    
    * force no header on aux version call
    
    ---------
    
    Co-authored-by: reiferschris <34450887+reiferschris@users.noreply.github.com>

--- src/OpencastApi/Rest/OcRestClient.php
+++ src/OpencastApi/Rest/OcRestClient.php
@@ -140,7 +140,10 @@ class OcRestClient extends Client
     {
         if (empty($this->version)) {
             try {
-                $defaultVersion = $this->performGet('/api/version/default');
+                // We have to use an aux object, in order to prevent overwriting arguments of current object.
+                $aux = clone $this;
+                $aux->enableNoHeader();
+                $defaultVersion = $aux->performGet('/api/version/default');
                 if (!empty($defaultVersion['body']) && isset($defaultVersion['body']->default)) {
                     $this->setVersion(str_replace(['application/', 'v', '+json'], ['', '', ''], $defaultVersion['body']->default));
                 } else {
