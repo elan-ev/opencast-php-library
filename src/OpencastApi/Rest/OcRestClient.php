1ad2ce3f5fc5a43c6fb894d821db4eb9f765043c
FarbodZamani <53179227+ferishili@users.noreply.github.com>
2024-09-20 10:57:04 +0200

    Fix unauthorised access to check the version as for aux object, (#29)
    * fix unauthorized access to check the version as for aux object,
    fixes #28
    
    * checker and tests improvements

--- src/OpencastApi/Rest/OcRestClient.php
+++ src/OpencastApi/Rest/OcRestClient.php
@@ -161,7 +161,11 @@ class OcRestClient extends Client
                 // We have to use an aux object, in order to prevent overwriting arguments of current object.
                 $aux = clone $this;
                 $aux->enableNoHeader();
-                $defaultVersion = $aux->performGet('/api/version/default');
+                $options = [];
+                if (!empty($this->username) && !empty($this->password)) {
+                    $options['auth'] = [$this->username, $this->password];
+                }
+                $defaultVersion = $aux->performGet('/api/version/default', $options);
                 if (!empty($defaultVersion['body']) && isset($defaultVersion['body']->default)) {
                     $this->setVersion(str_replace(['application/', 'v', '+json'], ['', '', ''], $defaultVersion['body']->default));
                 } else {
